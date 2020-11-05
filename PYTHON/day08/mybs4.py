e4fff2c5999680262ad6d6c96774d45e6380f2c6
dlsgur <dlsgur2323@gmail.com>
2020-11-05 13:35:21 +0900

    no message

--- PYTHON/day08/mybs4.py
+++ PYTHON/day08/mybs4.py
@@ -1,13 +1,12 @@
 import requests
 import bs4
 
-url = "http://192.168.43.123/JAVASCRIPT/list.html"
+url = "http://finance.daum.net/api/quote/A005930/sectors"
 
 response = requests.get(url)
 response.encoding="utf-8"
 html = response.text
 
-bs = bs4.BeautifulSoup(html, "lxml")
-tags = bs.select("td")
-for p in tags:
-    print(p.text)
+bs = bs4.BeautifulSoup(html, "json")
+
+print(bs)
--- PYTHON/day09/mystock.py
+++ PYTHON/day09/mystock.py
@@ -3,60 +3,63 @@ import bs4
 import pymysql
 from _datetime import datetime
 import time
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
+import threading
 
-db = pymysql.connect(host='localhost', port=3306,user='root',passwd='java', db='python_k', charset='utf8')
-        
-cursor = db.cursor()
-print("작업 시작")
-url = "https://www.ktb.co.kr/trading/popup/itemPop.jspx"
-response = requests.get(url)
-html = response.text
+from_class = uic.loadUiType("samsung.ui")[0]
 
-bs = bs4.BeautifulSoup(html, "lxml")
-tbody = bs.select("tbody")
-trs = tbody[0].select("tr")
+class MyWindow(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.btn.clicked.connect(self.click)
+        self.code = "005930"
+        self.name = "삼성전자"
+        
+        self.thre = threading.Thread(target=self.th)
+    
+    def click(self):
+        self.thre.start()
+        
+    def th(self):
+        while(True):
+            db = pymysql.connect(host='localhost', port=3306,user='root',passwd='java', db='python_k', charset='utf8')
+            cursor = db.cursor()
+            
+            now = datetime.now()
+            nowDateTime = now.strftime("%Y-%m-%d %H:%M")
+            
+            url = "https://finance.naver.com/item/sise.nhn?code="+self.code
+            response = requests.get(url)
+            html = response.text
+            
+            bs = bs4.BeautifulSoup(html, "lxml")
+            p = bs.select("p.no_today")
+            
+            if(len(p) > 0 ):
+                span = p[0].select("span")
+                price = span[0].text
+                
+                sql = "INSERT INTO stock (cname, ccode, price, gtime) VALUES (%s, %s, %s, %s)"
+                
+                cursor.execute(sql, (self.name, self.code, price, nowDateTime))
+                self.lbl.setText(price + " - " + nowDateTime)
+            db.commit()
+            db.close()
+            time.sleep(60)
+            
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec()
 
-codes = []
-for tr in trs:
-    td = tr.select("td")
-    code = td[0].text
-    name = td[1].text.rstrip()
-    arr = [name, code]
-    codes.append(arr)
-    
-l = len(codes)
 
-for ar in codes:
-    if(ar[0]=="삼성전자"):
-        code = ar[1]
-        name = ar[0]
-        
-while(True):
-    db = pymysql.connect(host='localhost', port=3306,user='root',passwd='java', db='python_k', charset='utf8')
-    cursor = db.cursor()
-    
-    now = datetime.now()
-    nowDateTime = now.strftime("%Y-%m-%d %H:%M")
-    
-    url = "https://finance.naver.com/item/sise.nhn?code="+code
-    response = requests.get(url)
-    html = response.text
-    
-    bs = bs4.BeautifulSoup(html, "lxml")
-    p = bs.select("p.no_today")
-    
-    if(len(p) > 0 ):
-        span = p[0].select("span")
-        price = span[0].text
-        
-        sql = "INSERT INTO stock (cname, ccode, price, gtime) VALUES (%s, %s, %s, %s)"
-        
-        cursor.execute(sql, (name, code, price, nowDateTime))
-        
-    print("작업 완료  -  "+nowDateTime)
-    db.commit()
-    db.close()
-    time.sleep(60)
+
+
+
 
 
     
